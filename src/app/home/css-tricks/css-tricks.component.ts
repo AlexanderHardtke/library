731afc05f491705ccd3ca91d9c2b8bb0cff323a6
Alexander Hardtke <5yl3kz@googlemail.com>
2024-12-28 21:45:59 +0100

    added more tricks

--- src/app/home/css-tricks/css-tricks.component.ts
+++ src/app/home/css-tricks/css-tricks.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-css-tricks',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './css-tricks.component.html',
   styleUrl: './css-tricks.component.scss'
 })
--- src/app/home/js-basics/js-basics.component.ts
+++ src/app/home/js-basics/js-basics.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-js-basics',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './js-basics.component.html',
   styleUrl: './js-basics.component.scss'
 })
