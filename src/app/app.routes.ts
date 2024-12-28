1ce8255161ed08bd3cf7395928489a4575ab1ae7
Alexander Hardtke <5yl3kz@googlemail.com>
2024-12-28 18:18:40 +0100

    Boxes ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CssTricksComponent } from './home/css-tricks/css-tricks.component';
+import { HomeComponent } from './home/home.component';
+import { HTMLComponent } from './home/html/html.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'CSS', component: CssTricksComponent },
+    { path: 'HTML', component: HTMLComponent },
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  components = [
+    {
+      name: "CSS Tricks",
+      useCase: "Some CSS styles",
+      url: "/CSS",
+    },
+    {
+      name: "HTML",
+      useCase: "Some HTML container",
+      url: "/HTML",
+    },
+    {
+      name: "CSS Tricks",
+      useCase: "Some CSS styles",
+      url: "/CSS",
+    },
+    {
+      name: "CSS Tricks",
+      useCase: "Some CSS styles",
+      url: "/CSS",
+    },
+  ]
+}
