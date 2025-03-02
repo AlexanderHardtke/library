c2c6dfa89b6bdccd4e6bd381eb6cdb4806d856fd
Alexander Hardtke <5yl3kz@googlemail.com>
2025-03-02 08:57:43 +0100

    update

--- src/app/home/back-end/back-end.component.ts
+++ src/app/home/back-end/back-end.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './back-end.component.scss'
 })
 export class BackEndComponent {
+  codeSnippet = `
+  REST_FRAMEWORK = {
+      'DEFAULT_PERMISSION_CLASSES': [
+          'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+      ],
+      'DEFAULT_AUTHENTICATION_CLASSES': [
+          'rest_framework.authentication.TokenAuthentication',
+      ],
+  }`;
+
+
+  codeSnippetpy = `
+  def __str__(self):<br>
+            return f"{self.first_name} {self.last_name}"<br>
+            in der Klasse<br>
+            customer = Customer.objects.all()[0]<br>
+            customer.order_set.all()`
 
 }
